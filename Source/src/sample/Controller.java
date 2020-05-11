62e5228939e13f8ecc9812d486ff752d39133b02
Ben Royans <ben.royans@gmail.com>
2020-05-11 08:23:51 +0800

    progress

--- Source/src/sample/Controller.java
+++ Source/src/sample/Controller.java
@@ -4,28 +4,40 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
-import javafx.scene.control.TableView;
-
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+//import com.gembox.spreadsheet.*;
 
 public class Controller
 {
-    private TableView tblMain;
-    private ObservableList<Row> data;
+    public TableView tblMain;
+//    private ObservableList<String[]> data;
 
 
     @FXML
     private void initialize() {
+//        tblMain.setItems(data);
+
+        TESTLOAD();
     }
+
+
 
     @FXML
     private void openCSVFile() {
@@ -44,7 +56,7 @@ public class Controller
             File selectedFile = fileChooser.getSelectedFile();
 
             // TEMP
-            System.out.println("Selected file: " + selectedFile.getName());
+            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
 
             // Load selected file
             loadCSVToTableView(selectedFile);
@@ -72,56 +84,69 @@ public class Controller
         }
     }
 
+    private void TESTLOAD()
+    {
+        File testFile = new File("C:\\Users\\Ben\\Documents\\GitHub\\JavaIII_AT2.6\\Source\\out\\production\\Source\\Test.csv");
+        loadCSVToTableView(testFile);
+    }
+
+    @FXML
     private void loadCSVToTableView(File file)
     {
-        // Clear TableView
-        initialiseTableView();
+        // Clear table view
+//        initialiseTableView();
 
-        try (FileReader fr = new FileReader(file))
+        try
         {
-            // Use 3rd party library to read .csv file into iterable data structure
-//            Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(fr);
-            Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(fr);
+            // Read .csv file
+            Reader reader = Files.newBufferedReader(Paths.get(file.getAbsolutePath()));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
 
+            // Adds a column for each header (first row elements)
+            CSVRecord headers = csvParser.iterator().next();
+            for (int i = 0; i < headers.size(); i++) {
+                addColumnToTable(headers.get(i));
+            }
 
-            // Print headers
-//            String[] headers = records.iterator().next().toString().split(",");
-//            for (String header: headers) {
-//                addColumnToTable(header);
-//            }
-
-
-            // Declare new row
-            Row newRow;
-
-            // For each row..
-            for (CSVRecord record : records)
+            // Get rows
+            ArrayList<String[]> rows = new ArrayList<>();
+            for (CSVRecord record : csvParser)
             {
-                // Create new row
-                newRow = new Row(record.size());
-
-                // For each cell in new row..
+                // Create String array
+                String[] newRow = new String[record.size()];
                 for (int i = 0; i < record.size(); i++)
                 {
-                    // TEMP DEBUG ONLY
+                    // Add cell contents to String array
+                    newRow[i] = record.get(i);
                     System.out.println(record.get(i));
-
-                    // Print value to row cell
-                    newRow.cells[i] = record.get(i);
                 }
 
-                // Add row to data
-                data.add(newRow);
+                // Add String array to list of rows
+                rows.add(newRow);
             }
-        }
-        catch (FileNotFoundException ex)
-        {
-            // Couldnt find given file
-            System.err.println("Load Error: Could not find .csv to load. Load failed.");
+
+            //  which will make your table view dynamic
+            ObservableList<ObservableList> csvData = FXCollections.observableArrayList();
+
+            for(String[] rowData : rows)
+            {
+                ObservableList<String> row = FXCollections.observableArrayList();
+
+                for(String cellData : rowData)
+                {
+                    row.add(cellData);
+                }
+
+                // add each row to cvsData
+                csvData.add(row);
+            }
+
+            // finally add data to tableview
+            tblMain.setItems(csvData);
         }
         catch (Exception ex)
         {
-            System.err.println("Load Error: " + ex.getMessage());
+            System.err.println("Error: " + ex.getLocalizedMessage());
         }
     }
 
@@ -131,22 +156,19 @@ public class Controller
         // ...
     }
 
-    private void initialiseTableView() {
-        // Create if doesnt exist
-        if (tblMain == null)
-        {
-            tblMain = new TableView();
+    public static TableView createTableView(String[] headers)
+    {
+        // Create new TableView
+        TableView tbl = new TableView();
+        tbl.setEditable(true);
+
+        // Create Columns
+        for (int i = 0; i < headers.length; i++) {
+            TableColumn newColumn = new TableColumn(headers[i]);
+            tbl.getColumns().add(newColumn);
         }
 
-        // Set data
-        data = FXCollections.observableArrayList();
-
-        // Set data
-        tblMain.setItems(data);
-
-//
-//        // Clear all data in the TableView
-//        tblMain.getItems().clear();
+        return tbl;
     }
 
 
@@ -154,8 +176,15 @@ public class Controller
     private void addColumnToTable(String column) {
         System.out.println("Adding header '" + column + "' to TableView.");
         TableColumn newColumn = new TableColumn(column);
-        newColumn.setCellValueFactory(new PropertyValueFactory<>(column));
+//        newColumn.setCellValueFactory(new PropertyValueFactory<>(column));
         tblMain.getColumns().add(newColumn);
+    }
+
+    @FXML
+    private void initialiseTableView()
+    {
+//        tblMain.getColumns().clear();
+//        tblMain.getItems().clear();
     }
 
 }
--- Source/src/sample/Main.java
+++ Source/src/sample/Main.java
@@ -1,12 +1,17 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+
+public class Main extends Application
+{
+    @FXML private TableView tblMain;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -18,5 +23,78 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+
+
+    public TableView createTableView(String[] headers)
+    {
+        // Create new TableView
+        TableView tbl = new TableView();
+        tbl.setEditable(true);
+
+        // Create Columns
+        for (int i = 0; i < headers.length; i++) {
+            TableColumn newColumn = new TableColumn(headers[i]);
+            tbl.getColumns().add(newColumn);
+        }
+
+        return tbl;
+    }
+
+
+
+
+    private MenuBar createMenuBar() {
+        // Create bar
+        MenuBar menuBar = new MenuBar();
+
+        // Create FILE menu
+        Menu menuFile = createFileMenu();
+        Menu menuHelp = createHelpMenu();
+
+        // Add menu(s) to menu bar
+        menuBar.getMenus().addAll(menuFile, menuHelp);
+
+        return menuBar;
+    }
+    private Menu createFileMenu() {
+        Menu file = new Menu();
+        file.setText("File");
+
+        // Populate FILE menu
+        MenuItem itmNew = new MenuItem("New");
+        file.getItems().add(itmNew);
+
+        file.getItems().add(new SeparatorMenuItem());
+
+        MenuItem itmLoad = new MenuItem("Load...");
+        file.getItems().add(itmLoad);
+
+        file.getItems().add(new SeparatorMenuItem());
+
+        MenuItem itmSave = new MenuItem("Save");
+        file.getItems().add(itmSave);
+        MenuItem itmSaveAs = new MenuItem("Save As..");
+        file.getItems().add(itmSaveAs);
+
+        file.getItems().add(new SeparatorMenuItem());
+        MenuItem itmExit = new MenuItem("Exit");
+        file.getItems().add(itmExit);
+
+        return file;
+    }
+    private Menu createHelpMenu() {
+        Menu help = new Menu();
+        help.setText("Help");
+
+        // Populate FILE menu
+        MenuItem itmNew = new MenuItem("Help");
+        help.getItems().add(itmNew);
+
+        MenuItem itmLoad = new MenuItem("About");
+        help.getItems().add(itmLoad);
+
+        return help;
     }
 }
